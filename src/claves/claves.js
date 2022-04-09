cfb126ca494e6b2203976c512b37e7f7c4f4153b
mark <59871561+mark-blazquez@users.noreply.github.com>
2022-04-09 19:49:21 +0200

    Delete src/claves directory

--- src/claves/claves.js
+++
@@ -1,7 +0,0 @@
-var Claves={
-    client_id : "1007987340759-mbug2d268foinfhjrktmsdeqmpmm3dp7.apps.googleusercontent.com",
-    client_secret :"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"
-}
-
-
-module.exports = {Claves};
